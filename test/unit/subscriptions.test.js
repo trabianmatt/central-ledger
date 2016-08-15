960c610190c324b1149a2d3d96f584d51f20f3bd
Nick Leeper <nickl@dwolla.com>
2016-08-15 15:09:55 -0500

    fix: update circle.yml

--- test/unit/subscriptions.test.js
+++ test/unit/subscriptions.test.js
@@ -6,6 +6,7 @@ const server = require('../../src/app.js')
 test('return subscription', function (t) {
   server.inject({ method: 'POST', url: '/subscriptions', payload: { } }, function (res) {
     t.equal(res.statusCode, 201)
+    t.equal(res.result.id, '12345')
     server.stop(t.end)
   })
 })
