28aa35c4d442c47698f2ea2e3e44ec81f3747796
Corey Oliver <corey@dwolla.com>
2016-08-24 15:17:09 -0500

    feat: migrate Hapi config to manifest file; this closes #4

--- src/server.js
+++ src/server.js
@@ -1,80 +1,27 @@
 'use strict'
 
-const Hapi = require('hapi')
-const Inert = require('inert')
-const Vision = require('vision')
-const HapiSwagger = require('hapi-swagger')
+const Glue = require('glue')
 const Massive = require('massive')
-const Pack = require('../package')
+const manifest = require('./manifest')
 
-let server = new Hapi.Server()
-server.connection({ port: 3000 })
+const composeOptions = { relativeTo: __dirname };
 
-let plugins = [
-  Inert,
-  Vision,
-  {
-    register: HapiSwagger,
-    options: {
-      info: {
-        'title': 'Central Ledger API Documentation',
-        'version': Pack.version
-      }
-    }
-  },
-  {
-    register: require('blipp')
-  },
-  {
-    register: require('good'),
-    options: {
-      reporters: {
-        console: [
-          {
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: [
-              {
-                response: '*',
-                log: '*',
-                error: '*'
-              }
-            ]
-          },
-          {
-            module: 'good-console',
-            args: [
-              {
-                format: 'YYYY-MM-DD HH:mm:ss.SSS'
-              }
-            ]
-          },
-          'stdout'
-        ]
-      }
-    }
-  },
-  {
-    register: require('./modules/subscriptions')
-  }
-]
-
-server.register(plugins, function (err) {
-  if (err) {
-    server.log(['error', 'plugins'], err)
-    throw err
-  }
-
-  server.start((err) => {
+module.exports = new Promise((resolve, reject) => {
+  Glue.compose(manifest, composeOptions, (err, server) => {
     if (err) {
-      server.log(['error', 'server'], err)
+      server.log(['error', 'plugins'], err)
       throw err
     }
 
-    server.app.db = Massive.connectSync({ connectionString: 'postgres://central_ledger:cVq8iFqaLuHy8jjKuA@localhost:5432/central_ledger' })
+    server.start((err) => {
+      if (err) {
+        server.log(['error', 'server'], err)
+        throw err
+      }
 
-    server.log('info', 'Server running at: ' + server.info.uri)
-  })
-})
+      server.app.db = Massive.connectSync({ connectionString: 'postgres://central_ledger:cVq8iFqaLuHy8jjKuA@localhost:5432/central_ledger' })
 
-module.exports = server
+      server.log('info', 'Server running at: ' + server.info.uri)
+    });
+  });
+});
