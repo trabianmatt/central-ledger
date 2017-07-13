622881252ab3f7cf7cbc2d4d35a4a3d82c272381
Nick Leeper <nleeper@users.noreply.github.com>
2017-07-13 11:30:47 -0500

    feat: add id and ilp optional fields for send message endpoint (#421)

--- src/api/messages/routes.js
+++ src/api/messages/routes.js
@@ -17,6 +17,8 @@ module.exports = [
       auth: Auth.strategy(),
       validate: {
         payload: {
+          id: Joi.string().optional(),
+          ilp: Joi.string().optional(),
           ledger: Joi.string().uri().required(),
           from: Joi.string().required(),
           to: Joi.string().required(),
